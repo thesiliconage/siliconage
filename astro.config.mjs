646771abddaccd352b23c6c811dbeb3d8db1ccac
SmartAI <ipandapanpan@gmail.com>
2026-06-13 22:15:08 +0800

    Set canonical URL to www.thesiliconage.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://thesiliconage.org',
+  site: 'https://www.thesiliconage.org',
   integrations: [sitemap()],
   output: 'static',
 });
